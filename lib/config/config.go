a22e48ba31a64d37415804ef137a5b471e0e12ce
Dreae <penitenttangentt@gmail.com>
2016-05-07 15:59:33 -0400

    Generate config on first run

--- lib/config/config.go
+++ lib/config/config.go
@@ -5,15 +5,25 @@ import (
   "encoding/json"
 )
 
+// Config holds master server configuration
 type Config struct {
   CACertFile string `json:"caCert"`
   CAKeyFile string `json:"caKey"`
   CertFile string `json:"cert"`
   KeyFile string `json:"key"`
   WebPort int `json:"webport"`
-  RpcPort int `json:"rpcport"`
+  RPCPort int `json:"rpcport"`
 }
 
+// DefaultConfig generates a config containing default settings
+func DefaultConfig() *Config {
+  return &Config{
+    WebPort: 8080,
+    RPCPort: 5455,
+  }
+}
+
+// ReadConfig reads the master server configuration from a file
 func ReadConfig(configPath string) (*Config, error) {
   configFile, err := os.Open(configPath)
   if err != nil {
@@ -27,4 +37,20 @@ func ReadConfig(configPath string) (*Config, error) {
   }
 
   return &config, nil
+}
+
+// WriteConfig saves the configuration to a file, truncating the file
+// if it exists
+func WriteConfig(configPath string, config *Config) error {
+  file, err := os.OpenFile(configPath, os.O_RDWR | os.O_TRUNC | os.O_CREATE, 0600)
+  if err != nil {
+    return err
+  }
+  defer file.Close()
+
+  encoder := json.NewEncoder(file)
+  if err = encoder.Encode(config); err != nil {
+    return err
+  }
+  return nil
 }
--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
   "fmt"
   "log"
   "flag"
+  "bufio"
+  "strconv"
   "net/http"
+  "github.com/dreae/colors"
 
   "github.com/dreae/erebus/lib/config"
   rpc "github.com/dreae/erebus/lib/rpc/server"
@@ -13,15 +16,19 @@ import (
 
 func main() {
   configPath := flag.String("c", "config.json", "Path to the config file")
+
+  var conf *config.Config
   _, err := os.Stat(*configPath)
   if err != nil {
-    log.Fatal("Config file doesn't exist: ", *configPath)
+    conf = firstTimeSetup()
+    if err = config.WriteConfig(*configPath, conf); err != nil {
+      panic(err)
+    }
+  } else {
+    if conf, err = config.ReadConfig(*configPath); err != nil {
+      log.Fatal("Error reading config: ", err)
+    }
   }
-  var conf *config.Config
-  if conf, err = config.ReadConfig(*configPath); err != nil {
-    log.Fatal("Error reading config: ", err)
-  }
-
   rpc.Init(conf)
 
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -32,4 +39,33 @@ func main() {
   } else {
     http.ListenAndServeTLS(fmt.Sprintf("0.0.0.0:%d", conf.WebPort), conf.CertFile, conf.KeyFile, nil)
   }
+}
+
+func firstTimeSetup() *config.Config {
+  baseConf := config.DefaultConfig()
+  reader := bufio.NewReader(os.Stdin)
+  fmt.Println("Config file doesn't exist, starting first run setup")
+
+  fmt.Println("Please enter a port for agent communication")
+  fmt.Printf("Port (%s): ", colors.Green(strconv.Itoa(baseConf.RPCPort)))
+  text, _ := reader.ReadString('\n')
+  if text != "" {
+    port, err := strconv.Atoi(text)
+    if err == nil {
+      baseConf.RPCPort = port
+    }
+
+  }
+
+  fmt.Println("Please enter a port for the web server to listen on")
+  fmt.Printf("Port (%s): ", colors.Green(strconv.Itoa(baseConf.WebPort)))
+  text, _ = reader.ReadString('\n')
+  if text != "" {
+    port, err := strconv.Atoi(text)
+    if err == nil {
+      baseConf.WebPort = port
+    }
+  }
+
+  return baseConf
 }
